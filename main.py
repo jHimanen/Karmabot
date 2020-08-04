ec4e99ac4c1fd259f38871b37ae4c81becfbcff2
Joel Himanen <joel.himanen@aalto.fi>
2020-08-04 23:55:29 +0300

    Hide bot token

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-updater = Updater(token="1347903578:AAHp5p8Sab7Xhp9Z4bujQhHhDmd80dVO0SE", use_context=True)
+updater = Updater(token="TOKEN", use_context=True)
 dispatcher = updater.dispatcher
 
 def start(update, context):{
