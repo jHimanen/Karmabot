3bf0bbc6cf9548494cc3cb1969147aec493a093f
Joel Himanen <joel.himanen@aalto.fi>
2020-08-05 11:12:14 +0300

    Minor fix to dummy bot

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(leve
 updater = Updater(token="TOKEN", use_context=True)
 dispatcher = updater.dispatcher
 
-def start(update, context):{
-    context.bot.send_message(chat_id=update.effective_chat_id, text="I bring the karma.")
-}
+def start(update, context):
+    update.message.reply_text("I bring the karma.")
+
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
